e2f5ebc1e1e6c472f5575c8394cff9dc6f099c24
miasung <mia.sung@mail.utoronto.ca>
2025-09-08 14:23:58 -0400

    Added 3 input parameters to multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,12 +2,16 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int multiple = 0;
-        for (int i = 1; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                multiple++;
+        System.out.println(multiples(1000,3,5));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
             }
         }
-        System.out.println(multiple);
+        return count;
     }
 }
